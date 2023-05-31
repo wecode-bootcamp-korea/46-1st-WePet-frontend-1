883b7e717ed2c6d347557785f9d43d787160fb6c
Yeon <yeon@Yeonui-MacBookPro.local>
2023-05-31 15:51:56 +0900

    ADD: ProductDetail 페이지 구현

--- src/pages/ProductDetail/ProductDetail.js
+++ src/pages/ProductDetail/ProductDetail.js
@@ -1,8 +1,30 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import './ProductDetail.scss'
+
+const data = {
+  title: 'WePet Food',
+  price: '3,500원',
+  productImg: '../../../public/images/still-life-pets-food-assortment.jpg',
+}
 
 const ProductDetail = () => {
-  return <h1>hello</h1>
+  return (
+    <div className="productDetail">
+      <div className="product">
+        <div className="productLeft">
+          <span>{data.title}</span>
+          <span>{data.price}</span>
+        </div>
+        <div className="productMainImg">
+          <img src={data.productImg} />
+        </div>
+        <div className="productRight"></div>
+      </div>
+    </div>
+  )
 }
 
 export default ProductDetail

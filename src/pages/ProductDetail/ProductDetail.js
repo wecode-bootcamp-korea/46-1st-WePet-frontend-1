7dddb539982da9093547474103699d61a3e22c7f
Yeon <yeon@Yeonui-MacBookPro.local>
2023-05-31 21:13:33 +0900

    ADD: ProductDetail 페이지 구현

--- src/pages/ProductDetail/ProductDetail.js
+++ src/pages/ProductDetail/ProductDetail.js
@@ -1,27 +1,91 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faChevronLeft,
+  faChevronRight,
+  faCartShopping,
+} from '@fortawesome/free-solid-svg-icons'
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
+import Count from './Count'
+import DetailInformation from './DetailInformation'
+import PRODUCT_DATA from './productData'
+import DETAILINFORMATION_DATA from './detailInformationData'
 import './ProductDetail.scss'
 
-const data = {
-  title: 'WePet Food',
-  price: '3,500원',
-  productImg: '../../../public/images/still-life-pets-food-assortment.jpg',
-}
-
 const ProductDetail = () => {
+  const detailImgArr = PRODUCT_DATA[0].detailImg
   return (
     <div className="productDetail">
       <div className="product">
         <div className="productLeft">
-          <span>{data.title}</span>
-          <span>{data.price}</span>
+          <p className="productName">{PRODUCT_DATA[0].title}</p>
+          <p className="price">{PRODUCT_DATA[0].price}</p>
         </div>
+
         <div className="productMainImg">
-          <img src={data.productImg} />
+          <FontAwesomeIcon
+            icon={faChevronLeft}
+            size="xl"
+            style={{ color: '#000000' }}
+            className="arrowLeft"
+          />
+          <img src={PRODUCT_DATA[0].productImg} alt="productImage" />
+          <FontAwesomeIcon
+            icon={faChevronRight}
+            size="xl"
+            style={{ color: '#000000' }}
+            className="arrowRight"
+          />
         </div>
-        <div className="productRight"></div>
+
+        <div className="productRight">
+          <div className="line" />
+          <p>배송정보</p>
+          <p className="grey">3,000원 &#40; 30,000원 이상 구매 시 무료&#41;</p>
+          <p className="grey">오후 1시 당일배송마감</p>
+          <div className="line" />
+          <div className="greyBox">
+            <p>{PRODUCT_DATA[0].title}</p>
+            <Count />
+          </div>
+          <div className="totalPrice">
+            <span>총 금액</span>
+            <span>3,500원</span>
+          </div>
+          <div className="shoppingBtn">
+            <div className="cartBtn">
+              <FontAwesomeIcon
+                icon={faCartShopping}
+                size="lg"
+                style={{ color: '#ffbb0f' }}
+                className="cartIcon"
+              />
+            </div>
+            <button className="buy">바로 구매하기</button>
+          </div>
+        </div>
+      </div>
+      <div className="rowLine" />
+
+      <div className="table">
+        <span className="greyTitle">상품정보</span>
+        <div className="columnLine" />
+        <span className="greyTitle">기본정보</span>
+        <div className="columnLine" />
+        <span className="greyTitle">상품후기</span>
+      </div>
+      <div className="rowLine" />
+
+      <div className="productImgs">
+        {detailImgArr.map((img, index) => {
+          // const imgValue = img.value
+          // console.log({ img })
+          return <img index={index} src={img} alt="productImages" />
+        })}
+      </div>
+      <div className="detailInformationBox">
+        <DetailInformation />
       </div>
     </div>
   )
---
+++ src/pages/ProductDetail/productData.js
@@ -0,0 +1,16 @@
+import img1 from './assets/강아지장난감1-1.avif'
+import img2 from './assets/강아지장난감1-2.avif'
+import img3 from './assets/강아지장난감1-3.avif'
+import img4 from './assets/강아지장난감1-4.avif'
+
+const PRODUCT_DATA = [
+  {
+    id: 1,
+    title: 'WePet Food',
+    price: '3,500원',
+    productImg: { img1 },
+    detailImg: [{ img1 }, { img2 }, { img3 }, { img4 }],
+  },
+]
+
+export default PRODUCT_DATA

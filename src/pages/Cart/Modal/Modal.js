109d9db41e60232aa1ff1d49523776e7d5586f71
AGNESBAEK <jw.agnes.baek@gmail.com>
2023-06-01 11:15:28 +0900

    ADD: Modal 팝업 시 검정 배경  레이아웃 추가

--- src/pages/Cart/Modal/Modal.js
+++ src/pages/Cart/Modal/Modal.js
@@ -3,22 +3,25 @@ import './Modal.scss'
 
 const Modal = ({ isModalOpen, setIsModalOpen }) => {
   return (
-    <div className="modal">
-      <div className="modalText">
-        <p>선택된 상품을 삭제하시겠습니까?</p>
+    <>
+      <div className="modal">
+        <div className="modalText">
+          <p>선택된 상품을 삭제하시겠습니까?</p>
+        </div>
+        <div className="modalBtn">
+          <button
+            className="cancelBtn"
+            onClick={() => {
+              setIsModalOpen(!isModalOpen)
+            }}
+          >
+            취소
+          </button>
+          <button className="deleteBtn">삭제하기</button>
+        </div>
       </div>
-      <div className="modalBtn">
-        <button
-          className="cancelBtn"
-          onClick={() => {
-            setIsModalOpen(!isModalOpen)
-          }}
-        >
-          취소
-        </button>
-        <button className="deleteBtn">삭제하기</button>
-      </div>
-    </div>
+      <div className="layer show" />
+    </>
   )
 }
 

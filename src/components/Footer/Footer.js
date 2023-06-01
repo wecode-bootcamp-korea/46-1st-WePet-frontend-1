504f9a64d95f6853b35eb0a136f34080322a6541
Yeon <yeon@Yeonui-MacBookPro.local>
2023-06-01 16:26:39 +0900

    FIX: 반복데이터 => 상수데이터 / map

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,56 +4,41 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faInstagram } from '@fortawesome/free-brands-svg-icons'
 
 import './Footer.scss'
+import FOOTER_DATA1 from './footerData1'
+import FOOTER_DATA2 from './footerData2'
 
 const Footer = () => {
   return (
-    <>
-      <div className="footer">
+    <div className="footer">
+      <Link to="/">
+        <div className="logo">WePet</div>
+      </Link>
+      <ul className="footerList">
+        {FOOTER_DATA1.map(data => {
+          return (
+            <Link to={data.link}>
+              <li>{data.name}</li>
+            </Link>
+          )
+        })}
         <Link to="/">
-          <div className="logo">WePet</div>
+          <li>
+            <FontAwesomeIcon
+              icon={faInstagram}
+              style={{ color: '#000000' }}
+              size="lg"
+              className="icon"
+            />
+            @wepet_store
+          </li>
         </Link>
-        <ul className="footerList">
-          <Link to="/">
-            <li>About</li>
-          </Link>
-          <Link to="/">
-            <li>공지사항</li>
-          </Link>
-          <Link to="/">
-            <li>이용약관</li>
-          </Link>
-          <Link to="/">
-            <li>개인정보처리방침</li>
-          </Link>
-          <Link to="/">
-            <li>대량구매/제휴안내</li>
-          </Link>
-          <Link to="/">
-            <li>
-              <FontAwesomeIcon
-                icon={faInstagram}
-                style={{ color: '#000000' }}
-                size="lg"
-                className="icon"
-              />
-              @wepet_store
-            </li>
-          </Link>
-        </ul>
-        <ul className="footerDetails">
-          <li>상호 : WePet</li>
-          <li>대표 : 백지율</li>
-          <li>사업자등록번호 : 123-45-67890</li>
-          <li>통신판매업신고번호: 2023-WePet-1234</li>
-          <li>사업자정보확인</li>
-          <li>대표번호 : 1234-5678</li>
-          <li>이메일 : wepet_store@wepet.com</li>
-          <li>주소 : 위팻시 위팻구 웨팻대로 123 위팻빌딩</li>
-          <li>호스팅제공 : WePet</li>
-          <li>© Wepet Corp. All rights reserved</li>
-        </ul>
-      </div>
-    </>
+      </ul>
+      <ul className="footerDetails">
+        {FOOTER_DATA2.map(data => {
+          return <li>{data.name}</li>
+        })}
+      </ul>
+    </div>
   )
 }
 
---
+++ src/components/Footer/footerData2.js
@@ -0,0 +1,34 @@
+const FOOTER_DATA2 = [
+  {
+    name: '상호 : WePet',
+  },
+  {
+    name: '대표 : 백지율',
+  },
+  {
+    name: '사업자등록번호 : 123-45-67890',
+  },
+  {
+    name: '통신판매업신고번호: 2023-WePet-1234',
+  },
+  {
+    name: '사업자정보확인',
+  },
+  {
+    name: '대표번호 : 1234-5678',
+  },
+  {
+    name: '이메일 : wepet_store@wepet.com',
+  },
+  {
+    name: '주소 : 위팻시 위팻구 웨팻대로 123 위팻빌딩',
+  },
+  {
+    name: '호스팅제공 : WePet',
+  },
+  {
+    name: '© Wepet Corp. All rights reserved',
+  },
+]
+
+export default FOOTER_DATA2

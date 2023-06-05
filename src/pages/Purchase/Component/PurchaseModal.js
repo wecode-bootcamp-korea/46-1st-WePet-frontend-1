0592ee161fb3df051e64114d5758240e74176a56
Yeon <yeon@Yeonui-MacBookPro.local>
2023-06-05 14:46:23 +0900

    ADD: purchaseModal 구현 중

--- src/pages/Purchase/Component/PurchaseModal.js
+++ src/pages/Purchase/Component/PurchaseModal.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCashRegister } from '@fortawesome/free-solid-svg-icons'
+import './PurchaseModal.scss'
 
 const PurchaseModal = () => {
   return (
     <div className="purchaseModal">
       <div className="purchaseModalContent">
-        <div className="title">결제가 완료되었습니다 !</div>
+        <div className="title">
+          <div className="line">
+            <FontAwesomeIcon icon={faCashRegister} className="icon" size="lg" />
+            결제가 완료되었습니다 !
+          </div>
+          <div className="ham lineLeft"></div>
+          <div className="ham lineRight"></div>
+        </div>
       </div>
     </div>
   )

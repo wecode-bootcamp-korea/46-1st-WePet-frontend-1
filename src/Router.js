3d0c2f0d2a8c1e7098fca8f715189458499edb9c
AGNESBAEK <jw.agnes.baek@gmail.com>
2023-06-05 20:21:27 +0900

    ADD: router 추가

--- src/Router.js
+++ src/Router.js
@@ -24,6 +24,7 @@ function Router() {
         <Route path="/purchase" element={<Purchase />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/mypage" element={<MyPage />} />
+        <Route path="/mypage:id" element={<MyPage />} />
       </Routes>
       <Footer />
     </BrowserRouter>

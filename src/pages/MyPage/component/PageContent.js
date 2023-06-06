8e4d9ba2d4005d4431108443fbd3c1e3aace1107
AGNESBAEK <jw.agnes.baek@gmail.com>
2023-06-06 22:54:27 +0900

    ADD: 포인트 컴포넌트 추가

--- src/pages/MyPage/component/PageContent.js
+++ src/pages/MyPage/component/PageContent.js
@@ -4,6 +4,34 @@ import './PageContent.scss'
 
 const PageContent = ({ title, content, button }) => {
   const [isModalOpen, setIsModalOpen] = useState(false)
+  if (title === '포인트') {
+    return (
+      <div className="pageContent">
+        <div className="pageContentHeader">
+          <h1 className="pageContentTitle">{title}</h1>
+          {button && (
+            <button
+              onClick={() => {
+                setIsModalOpen(!isModalOpen)
+              }}
+            >
+              {button}
+            </button>
+          )}
+        </div>
+        <div className="pointList">
+          <div className="pointBox">
+            <h4 class="point">500,000 point</h4>
+            <div class="condition"> 신규 회원 부여 포인트 </div>
+            <div class="conditionDetail">
+              2023년 6월 30일까지 <br /> 사용 가능
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+
   return (
     <div className="pageContent">
       <div className="pageContentHeader">

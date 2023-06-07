70cf694e992555090750cc79e1ecf5136f805453
AGNESBAEK <jw.agnes.baek@gmail.com>
2023-06-07 10:57:39 +0900

    MODIFY: 리뷰 수정 반영

--- src/pages/MyPage/component/PageContent.js
+++ src/pages/MyPage/component/PageContent.js
@@ -4,39 +4,10 @@ import './PageContent.scss'
 
 const PageContent = ({ title, content, button }) => {
   const [isModalOpen, setIsModalOpen] = useState(false)
-  if (title === '포인트') {
-    return (
-      <div className="pageContent">
-        <div className="pageContentHeader">
-          <h1 className="pageContentTitle">{title}</h1>
-          {button && (
-            <button
-              className="modalBtn"
-              onClick={() => {
-                setIsModalOpen(!isModalOpen)
-              }}
-            >
-              {button}
-            </button>
-          )}
-        </div>
-        <div className="pointList">
-          <div className="pointBox">
-            <h4 class="point">500,000 point</h4>
-            <div class="condition"> 신규 회원 부여 포인트 </div>
-            <div class="conditionDetail">
-              2023년 6월 30일까지 <br /> 사용 가능
-            </div>
-          </div>
-        </div>
-      </div>
-    )
-  }
-
   return (
     <div className="pageContent">
       <div className="pageContentHeader">
-        {isModalOpen && (
+        {title !== '포인트' && isModalOpen && (
           <div className="modalLayout">
             <div className="modalLayer" />
             <MyPageModal setIsModalOpen={setIsModalOpen} />
@@ -54,10 +25,23 @@ const PageContent = ({ title, content, button }) => {
           </button>
         )}
       </div>
-      <div className="pageContentText">
-        <h3 className="exclamation">앗!</h3>
-        <div className="text">{content}</div>
-      </div>
+
+      {title !== '포인트' ? (
+        <div className="pageContentText">
+          <h3 className="exclamation">앗!</h3>
+          <div className="text">{content}</div>
+        </div>
+      ) : (
+        <div className="pointList">
+          <div className="pointBox">
+            <h4 className="point">500,000 point</h4>
+            <div className="condition">신규 회원 부여 포인트</div>
+            <div className="conditionDetail">
+              2023년 6월 30일까지 <br /> 사용 가능
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
